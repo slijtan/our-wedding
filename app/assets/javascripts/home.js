64cc2d69f95f7d5c001d98b51a892b1b44db05e3
Lijen Tan <lijentan@gmail.com>
2015-02-01 01:29:38 -0800

    working basic background

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    alert('javascript working');
+    resize_components();
+    window.scrollTo(0,document.body.scrollHeight); // scroll to bottom
+});
+
+function resize_components() {
+    var pageHeightPercentage = 1.5;
+    var textDistanceFromTop = 0.45;
+
+    var windowHeight = $( window ).height();
+    var pageHeight =  pageHeightPercentage * windowHeight;
+    var textMarginTop = textDistanceFromTop * pageHeight;
+
+    $('.page').height(pageHeight);
+    console.log(textMarginTop);
+    $('.page .text').css( "margin-top", textMarginTop );
+}
+
+
+$( window ).resize(function() {
+    resize_components();
 });
