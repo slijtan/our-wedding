760ff84d0dd9d90a8a0beb2631dc18bf1fa06839
Lijen Tan <lijentan@gmail.com>
2015-02-01 21:31:37 -0800

    sprite movement by percentage working

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,22 +1,63 @@
-$(document).ready(function() {
-    resize_components();
-    window.scrollTo(0,document.body.scrollHeight); // scroll to bottom
-});
+var $window = $(window);
+var documentHeight;
+var windowHeight;
 
-function resize_components() {
+function resizeComponents() {
+    documentHeight = $(document).height();
+    windowHeight = $window.height();
+
     var pageHeightPercentage = 1.5;
     var textDistanceFromTop = 0.45;
 
-    var windowHeight = $( window ).height();
     var pageHeight =  pageHeightPercentage * windowHeight;
     var textMarginTop = textDistanceFromTop * pageHeight;
 
     $('.page').height(pageHeight);
-    console.log(textMarginTop);
     $('.page .text').css( "margin-top", textMarginTop );
+
+    documentHeight = $(document).height();
+    windowHeight = $window.height();
 }
 
+function scrollToBottom() {
+    window.scrollTo(0, document.body.scrollHeight);
+}
 
-$( window ).resize(function() {
-    resize_components();
+function initializeSprites() {
+    var timeline = new TimelineMax({ paused: true });
+    initializeVerticalSprite(timeline, "#bird", 100, 0)
+
+    $window.on("scroll", function() {
+	var scrollTop = $window.scrollTop();
+	var scrollPercent = 1.0 - (scrollTop / (documentHeight - windowHeight));
+	scrollPercent = Math.max(0, scrollPercent);
+	scrollPercent = Math.min(1, scrollPercent);
+
+	console.log("------------------------------------");
+	console.log("Scroll percent: " + scrollPercent);
+	console.log("Document Height: " + documentHeight);
+	console.log("Scroll Top: " + scrollTop);
+	console.log("Bird position: " + $("#bird").css('top'));
+
+	timeline.progress(scrollPercent).pause();
+    });
+}
+
+function initializeVerticalSprite(timeline, selector, startPercentage, endPercentage) {
+    topMax = documentHeight - $(selector).height();
+    topStart = topMax * startPercentage / 100;
+    topEnd = topMax * endPercentage / 100;
+
+    $(selector).css({ top: startPercentage + "%" });
+    timeline.to(selector, 100, { top: endPercentage + "%" });
+}
+
+$(document).ready(function() {
+    resizeComponents();
+    initializeSprites();
+    scrollToBottom();
+
+    $( window ).resize(function() {
+	resizeComponents();
+    });
 });
